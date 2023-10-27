2376ce18213432fbed68f4d580b2b5366d47de1f
Mostafa Milly <57003058+MostafaKMilly@users.noreply.github.com>
2023-10-27 20:09:08 +0300

    Change og image

--- src/utils/createOgImage.ts
+++ src/utils/createOgImage.ts
@@ -2,21 +2,21 @@ export const createOgImage = ({ title, meta }: { title: string; meta: string }) 
   [
     // ACCOUNT PREFIX
     // Add your own Cloudinary account ID.
-    `https://res.cloudinary.com/<CLOUDINARY ID>/image/upload`,
+    `https://res.cloudinary.com/ds9a7nckr/image/upload`,
     // Composed Image Transformations
     `w_1600,h_836,q_100`,
     // TITLE
     // Karla google font in light rose
-    `l_text:Ubuntu_92_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_1200,h_400`,
+    `l_text:Ubuntu_92_bold:${e(title)},co_rgb:FFFFFF,c_fit,w_1200,h_400`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_340`,
     // META
     // Karla, but smaller
-    `l_text:Ubuntu_52_bold:${e(meta)},co_rgb:ffe4e680,c_fit,w_1400`,
+    `l_text:Ubuntu_52_bold:${e(meta)},co_rgb:FFFFFF80,c_fit,w_1400`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_100`,
     // IMAGE
-    `<YOUR IMAGE ID>`,
+    `Frame_1_g3ty71.png`,
   ].join('/');
 
 // double escape for commas and slashes
